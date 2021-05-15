d31068378dcee17b0585826567039fcd2368c30c
Isabella Pham <isabella.aivy.pham@gmail.com>
2021-05-15 18:08:28 -0400

    still figuring out dynamic web design

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-import '../style/home.css';
+import styles from '../style/home.css';
 import KimiNoNaWaBackgroundVideo from '../components/KimiNoNaWaBackgroundVideo/KimiNoNaWaBackgroundVideo'
 import IntroductionSideBar from '../components/IntroductionSideBar/IntroductionSideBar'
 
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.home}>
   			<KimiNoNaWaBackgroundVideo />
         <IntroductionSideBar />
       </div>
